ca45452b69467e1b647e9f9cf5a38b2e9c47c16f
Yan1less <895072956@qq.com>
2019-04-03 22:23:41 +0800

    first commit

---
+++ src/test/java/com/microweather/mvn_zuul/MvnZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.microweather.mvn_zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvnZuulApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
